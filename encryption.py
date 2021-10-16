38000eefda066d060f8d0a508634f4813fed98f5
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-10-16 17:30:36 +1300

    Removed package structure and added encryption module.

---
+++ encryption.py
@@ -0,0 +1,16 @@
+def encrypt(message: str) -> str:
+    """
+
+    :param message:
+    :return:
+    """
+    print(message)
+
+
+def decrypt(message: str) -> str:
+    """
+
+    :param message:
+    :return:
+    """
+    print(message)
