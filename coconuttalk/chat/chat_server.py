56f05e6ae790812cd0633bb49da8e58c6f285fab
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-10-18 17:03:12 +1300

    Implemented updated tuple style of transmission and added room interactions.

--- coconuttalk/chat/chat_server.py
+++ coconuttalk/chat/chat_server.py
@@ -56,7 +56,7 @@ class ChatServer:
             try:
                 readable, writeable, exceptional = select.select(
                     inputs, self.outputs, [])
-            except select.error as e:
+            except select.error:
                 break
 
             for sock in readable:
@@ -67,18 +67,21 @@ class ChatServer:
                     print(
                         f'Chat server: got connection {client.fileno()} from {address}')
                     # Read the login name
-                    cname = receive(client).split('NAME: ')[1]
+                    cname: str = receive(client)[1]
 
                     # Compute client name and send back
                     self.clients += 1
-                    send(client, f'CLIENT: {str(address[0])}:{str(address[1])}')
+                    print(address)
+                    send(client, "CLIENT", address)
+                    # send(client, f'CLIENT: {str(address[0])}:{str(address[1])}')
                     inputs.append(client)
 
                     self.client_map[client] = (address, cname, time.time())
                     # Send joining information to other clients
-                    msg = f'\n(Connected: New client ({self.clients}) from {self.get_client_name(client)})'
-                    for output in self.outputs:
-                        send(output, msg)
+                    # msg = f'\n(Connected: New client ({self.clients}) from {self.get_client_name(client)})'
+                    # for output in self.outputs:
+                    #     send(sock, "CLIENTLIST", *self.client_map.values())
+                        # send(output, msg)
                     self.outputs.append(client)
 
                 elif sock == sys.stdin:
@@ -90,47 +93,12 @@ class ChatServer:
                     elif cmd == 'quit':
                         running = False
                 else:
-                    # handle all other sockets
+                    # handle all other messages into the server's socket.
                     try:
                         data = receive(sock)
-                        print("data: " + data)
-                        if data == "GET_ALL_CLIENTS":
-                            send_clients(sock, self.client_map.values())
+                        print(f"data: {data}")
 
-                        elif data.startswith("CREATEROOM:"):
-                            _, room_name, chatting_client_port = data.split(":")
-                            message = ""
-                            try:
-                                _ = self.rooms[room_name]
-                                message = "Room with given name already exists. Please try a different name."
-                            except KeyError:
-                                self.rooms[room_name] = [sock]
-                                if chatting_client_port != "null":
-                                    self.rooms[room_name].append(self.get_client_socket(int(chatting_client_port)))
-                                message = "SUCCESS"
-                            finally:
-                                print(message + f"\nRoom: {room_name} has been created.")
-                                send(sock, message)
-
-                        elif data.startswith("CLOSEROOM:"):
-                            _, room_name = data.split(":")
-                            try:
-                                self.rooms.pop(room_name)
-                            except KeyError as e:
-                                print(f"Room: {room_name} does not exist.")
-                                _ = e
-                            print(f"Room: {room_name} has been deleted.")
-
-                        elif data:
-                            # Send as new client's message...
-                            # msg = f'{self.get_client_name(sock)}:{data}'
-                            msg = format_message(self.get_client_name(sock), data)
-
-                            # Send data to all except ourself
-                            for output in self.outputs:
-                                if output != sock:
-                                    send(output, msg)
-                        else:
+                        if data == ():
                             print(f'Chat server: {sock.fileno()} hung up')
                             self.clients -= 1
                             sock.close()
@@ -138,11 +106,76 @@ class ChatServer:
                             self.outputs.remove(sock)
 
                             # Sending client leaving information to others
-                            msg = f'\n(Now hung up: Client from {self.get_client_name(sock)})'
+                            # msg = f'\n(Now hung up: Client from {self.get_client_name(sock)})\n'
                             self.client_map.pop(sock)
 
+                            # this change will soon be detected by clients. No need to manually update again.
+                            # for output in self.outputs:
+                            #     send(output, msg)
+
+                        elif data[0] == "GET_ALL_CLIENTS":
+                            send(sock, *self.client_map.values())
+
+                        # Convention: ("CREATEROOM", "room_name_hehe_xd", 13531)
+                        elif data[0] == "CREATEROOM":
+                            _, room_name, chatting_client_port = data
+                            message = ""
+                            return_code = ""
+                            try:
+                                _ = self.rooms[room_name]
+                                message = "Room with given name already exists. Please try a different name."
+                                return_code = "EXISTS"
+                            except KeyError:
+                                self.rooms[room_name] = [sock]
+                                if chatting_client_port != -1:
+                                    self.rooms[room_name].append(self.get_client_socket(chatting_client_port))
+                                message = f"Room: {room_name} has been created."
+                                return_code = "SUCCESS"
+                            finally:
+                                print(message)
+                                send(sock, return_code)
+
+                        elif data[0] == "JOINROOM":
+                            _, room_name = data
+                            return_code = ""
+
+                            try:
+                                self.rooms[room_name].append(sock)
+                                return_code = "SUCCESS"
+                            except KeyError:
+                                return_code = "FAIL"
+                            finally:
+                                send(sock, return_code)
+
+                        # Convention: ("CLOSEROOM", "room_name_hehe_xd")
+                        elif data[0] == "EXITROOM":
+                            # Change this.
+                            _, room_name = data
+
+                            try:
+                                self.rooms[room_name].remove(sock)
+                                if not self.rooms[room_name]:
+                                    self.rooms.pop(room_name)
+                            except KeyError:
+                                print(f"Room: {room_name} does not exist.")
+
+                            send(sock, "EXITROOM")
+                            print(f"Room: {room_name} has been deleted.")
+
+                        # Convention: ("MESSAGE", "Hello, World!")
+                        elif data[0] == "MESSAGE":
+                            # Send as new client's message...
+                            # msg = f'{self.get_client_name(sock)}:{data}'
+                            msg = format_message(self.get_client_name(sock), data[1])
+
+                            # Send data to all except ourself
                             for output in self.outputs:
-                                send(output, msg)
+                                if output != sock:
+                                    send(output, msg)
+
+                        else:
+                            print("Not sure what happened; Something strange happened in chat_server.py?")
+
                     except socket.error:
                         # Remove
                         inputs.remove(sock)
