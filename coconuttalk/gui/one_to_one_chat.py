abd274594f47c4a6f0f278969f0adf559c96074c
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-10-18 17:03:55 +1300

    Fixed bugs regarding 1:1 chat screen.

--- coconuttalk/gui/one_to_one_chat.py
+++ coconuttalk/gui/one_to_one_chat.py
@@ -1,14 +1,16 @@
 import time
-from threading import Thread
-
-from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QDialog, QTextBrowser, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QPushButton
 
 from coconuttalk.chat.chat_client import ChatClient
 from coconuttalk.chat.utils import send
+from coconuttalk.gui.fetch_utils import FetchMessage
 
 
 class OneToOneChatWidget(QDialog):
+    """
+    This is a 1:1 chat window which allows users to chat with a single person.
+    """
+
     def __init__(self, other_client_nickname: str, other_client_port: int, client: ChatClient,
                  parent=None) -> None:
         super().__init__(parent)
@@ -25,80 +27,57 @@ class OneToOneChatWidget(QDialog):
 
         self.init_ui()
 
-        self.fetch_message_thread = FetchMessage(client=self.client)
-        self.fetch_message_thread.chat_fetched.connect(self.add_chat)
+        self.fetch_message_thread = FetchMessage(client=self.client, parent=self)
+        self.fetch_message_thread.chat_fetched.connect(self.chats.append)
         self.fetch_message_thread.start()
 
-        # self.fetch_message_thread_alive = True
-        # self.fetch_message_thread = Thread(target=self.fetch_message, args=(lambda: self.fetch_message_thread_alive,))
-        # self.fetch_message_thread.start()
-
     def init_ui(self) -> None:
+        """
+        Initializes UI of the window.
+        """
+        # Set up main layout
         main_layout = QVBoxLayout()
         self.setLayout(main_layout)
 
+        # Add main header of the window.
         main_layout.addWidget(QLabel(f"Chat with {self.other_client_nickname}"))
         main_layout.addWidget(self.chats)
 
+        # Add the chat container with list of chats
         chats_container = QHBoxLayout()
+
+        # Add send button and input
         self.send_button.clicked.connect(self.send)
         chats_container.addWidget(self.chats_input, 7)
         chats_container.addWidget(self.send_button, 3)
         main_layout.addLayout(chats_container)
 
+        # Add close button
         close_button = QPushButton("Close", self)
         close_button.clicked.connect(self.close_chat)
         main_layout.addWidget(close_button)
 
-    # def fetch_message(self, is_thread_alive) -> None:
-    #     print("Start Thread!")
-    #     while True:
-    #         time.sleep(2)
-    #         print("yes, thread is running...")
-    #         self.chats.append("James (08:23): Yes, thread is running...")
-    #         if not is_thread_alive():
-    #             break
-    #     print("thread ended.")
-
-    def add_chat(self, message) -> None:
-        print(message)
-        self.chats.append(message)
-
     def close_chat(self) -> None:
         """
         Removes the chat in the server, then closes the chat.
-        :return: Remove chat
         """
         self.fetch_message_thread.stop()
-        send(self.client.sock, f"CLOSEROOM:{self.client.connected_port}_to_{self.other_client_port}")
+        self.client.leave_room(f"{self.client.connected_port}_to_{self.other_client_port}")
+        # send(self.client.sock, f"CLOSEROOM:{self.client.connected_port}_to_{self.other_client_port}")
         self.accept()
 
     def send(self) -> None:
+        """
+        Sends a message in chat input field to the chatting client.
+        """
+        # Retrieve message and clear chat input.
         message: str = self.chats_input.text()
+        self.chats_input.clear()
         print("Message sent: " + message)
+
+        # Only send message if it's not empty.
         if message:
-            send(self.client.sock, message)
+            # Convention: ("MESSAGE", "Hello, world!")
+            send(self.client.sock, "MESSAGE", message)
             current_time = time.strftime("%H:%M", time.localtime())
             self.chats.append(f"Me ({current_time}): {message}")
-
-
-class FetchMessage(QThread):
-    chat_fetched = pyqtSignal(object)
-
-    def __init__(self, client: ChatClient, parent=None):
-        super(FetchMessage, self).__init__(parent)
-        self.thread_alive = True
-        self.client = client
-
-    def run(self):
-        print("Start Thread!")
-        while self.thread_alive:
-            messages: list[str] = self.client.fetch_messages()
-            for message in messages:
-                if message.startswith("SHUTDOWN:"):
-                    self.chat_fetched.emit()
-            [self.chat_fetched.emit(message) for message in messages]
-            # self.chats.append("James (08:23): Yes, thread is running...")
-
-    def stop(self):
-        self.thread_alive = False
