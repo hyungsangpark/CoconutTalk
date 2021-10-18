0c14e396f58cad0ecbb5584fe66101d82a6c0453
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-10-18 17:04:55 +1300

    Removed deprecated code and changed transfer style to tuple from string.

--- coconuttalk/chat/utils.py
+++ coconuttalk/chat/utils.py
@@ -6,16 +6,16 @@ import struct
 # ((ip_addresss, port), name, connected_time)
 import time
 
-Client = tuple[tuple[str, int], str, float]
+Address = tuple[str, int]
+Client = tuple[Address, str, float]
 SERVER_HOST = "localhost"
 
 
 def send(channel: socket.socket, *args) -> None:
     """
-
-    :param channel:
-    :param args:
-    :return:
+    Sends an object through the socket provided.
+    :param channel: Socket to send the data
+    :param args: Arguments in tuple.
     """
     buffer: bytes = pickle.dumps(args)
     value: int = socket.htonl(len(buffer))
@@ -24,43 +24,47 @@ def send(channel: socket.socket, *args) -> None:
     channel.send(buffer)
 
 
-def receive(channel: socket.socket) -> str:
+def receive(channel: socket.socket) -> tuple:
     """
+    Receives an object through the socket provided.
 
-    :param channel:
-    :return:
+    Note that since the receiving object can be of any form of tuple, usage of this should be highly monitored.
+    If there was an error in receiving data, an empty tuple will be returned.
+
+    :param channel: Socket to receive the data.
+    :return: A tuple of objects received from the socket. If there were error in receiving data, empty tuple; ().
     """
     size = struct.calcsize("L")
     size = channel.recv(size)
     try:
         size = socket.ntohl(struct.unpack("L", size)[0])
     except struct.error:
-        return ''
-    buf = ""
-    while len(buf) < size:
-        buf = channel.recv(size - len(buf))
-    return pickle.loads(buf)[0]
-
-
-def send_clients(channel, list_clients):
-    buffer = pickle.dumps(list(list_clients))
-    value = socket.htonl(len(buffer))
-    size = struct.pack("L", value)
-    channel.send(size)
-    channel.send(buffer)
-
-
-def receive_clients(channel):
-    size = struct.calcsize("L")
-    size = channel.recv(size)
-    try:
-        size = socket.ntohl(struct.unpack("L", size)[0])
-    except struct.error as e:
-        return ''
+        return ()
     buf = ""
     while len(buf) < size:
         buf = channel.recv(size - len(buf))
     return pickle.loads(buf)
+
+
+# def send_clients(channel, list_clients):
+#     buffer = pickle.dumps(list(list_clients))
+#     value = socket.htonl(len(buffer))
+#     size = struct.pack("L", value)
+#     channel.send(size)
+#     channel.send(buffer)
+
+
+# def receive_clients(channel):
+#     size = struct.calcsize("L")
+#     size = channel.recv(size)
+#     try:
+#         size = socket.ntohl(struct.unpack("L", size)[0])
+#     except struct.error as e:
+#         return ''
+#     buf = ""
+#     while len(buf) < size:
+#         buf = channel.recv(size - len(buf))
+#     return pickle.loads(buf)
 
 
 def format_message(nickname: str, message: str) -> str:
